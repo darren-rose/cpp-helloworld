4021da12e478b65670bb1445ce5ecf04baefd39a
darren-rose <darrenwrose@gmail.com>
2020-12-23 21:50:24 +0100

    Initial Commit

---
+++ helloworld/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  helloworld
+//
+//  Created by Darren Rose on 23/12/2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
