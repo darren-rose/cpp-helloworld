bb3032dfab404d1eab3f6493b7800d090c54f3ee
darren-rose <darrenwrose@gmail.com>
2021-02-24 00:13:59 +0100

    feat(main): hello world

---
+++ Makefile
@@ -0,0 +1,6 @@
+build: main.cpp
+	c++ -std=c++11 -o helloworld main.cpp
+
+clean:
+	rm -f helloworld
+
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char *argv[]) {
+    cout << "Hello, World!" << endl;
+    cerr << argc << endl;
+    cerr << argv[0] << endl;
+    return 0;
+}
